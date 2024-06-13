9cf385367ae3468267f747f320b01805aeeed84e
Prakash-Kani <prakashkoffi@gmail.com>
2024-06-13 10:49:00 +0530

    Added Conditions for Left Click Gesture

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import mediapipe as mp
 import numpy as np
 import cv2
 import pyautogui
+from pynput.mouse import Button, Controller
 
 from math_exp import get_angle, get_distance
 import gesture as ges
+
+
+mouse = Controller()
 
 # Initialize MediaPipe Hands.
 mp_hands = mp.solutions.hands
@@ -19,55 +23,77 @@ camera = cv2.VideoCapture(0)
 # Get the size of the primary monitor.
 screen_width, screen_height = pyautogui.size()
 
+def gesture_recognition(image, landmark_lst,  ):
 
-try:
+    thump_fig_dist = get_distance(landmark_lst[4], landmark_lst[5])
+
+    cursor =ges.to_move_cursor(ges.to_find_index_finger_tip(landmark_lst), thump_fig_dist)
+
+    #Cursor
+    if cursor[2]:
+        x = cursor[0]
+        y = cursor[1]
+        pyautogui.moveTo(x, y, duration =0.2)
+        cv2.putText(image, "Cursor", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    
+    # Left Click
+    elif ges.to_left_click(landmark_lst, thump_fig_dist):
         
-    while camera.isOpened():
-        success, image = camera.read()
-        if not success:
-            break
-        elif success:
+        mouse.press(Button.left)
+        mouse.release(Button.left)
+        cv2.putText(image, "Left Click", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-            image = cv2.flip(image, 1)
 
-            # Convert the BGR image to RGB.
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-            # Process the image and find hands.
-            result = hands.process(image)
 
-            # Convert the image back to BGR.
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            landmark_lst=[]
 
-            if result.multi_hand_landmarks:
+def main():
+    try:
+            
+        while camera.isOpened():
+            success, image = camera.read()
+            if not success:
+                break
+            elif success:
 
-                mp.solutions.drawing_utils.draw_landmarks(image, result.multi_hand_landmarks[0], mp_hands.HAND_CONNECTIONS)
-                
+                image = cv2.flip(image, 1)
 
-                for landmarks in result.multi_hand_landmarks[0].landmark:
-                    landmark_lst.append((landmarks.x, landmarks.y, landmarks.z))
+                # Convert the BGR image to RGB.
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-                thump_fig_dist = get_distance(landmark_lst[4], landmark_lst[5])
-                
-                cursor =ges.to_move_cursor(ges.to_find_index_finger_tip(landmark_lst), thump_fig_dist)
+                # Process the image and find hands.
+                result = hands.process(image)
 
-                if cursor[2]:
-                    x = cursor[0]
-                    y = cursor[1]
-                    pyautogui.moveTo(x, y, duration =0.2)
-                    cv2.putText(image, "Cursor", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                # Convert the image back to BGR.
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            # Display the image.
-            cv2.imshow('Hand Tracking and Gesture Recognition', image)
-            if cv2.waitKey(5) & 0xFF == 27:
-                break  
+                landmark_lst=[]
 
-except Exception as e:
-    print('Exception :', e )
+                if result.multi_hand_landmarks:
 
-finally:
-    camera.release()
-    cv2.destroyAllWindows()
+                    mp.solutions.drawing_utils.draw_landmarks(image, result.multi_hand_landmarks[0], mp_hands.HAND_CONNECTIONS)
+                    
 
+                    for landmarks in result.multi_hand_landmarks[0].landmark:
+                        landmark_lst.append((landmarks.x, landmarks.y, landmarks.z))
+
+                    
+                    
+                    gesture_recognition(image, landmark_lst )
+
+                # Display the image.
+                cv2.imshow('Hand Tracking and Gesture Recognition', image)
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break  
+
+    except Exception as e:
+        print('Exception :', e )
+
+    finally:
+        camera.release()
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()

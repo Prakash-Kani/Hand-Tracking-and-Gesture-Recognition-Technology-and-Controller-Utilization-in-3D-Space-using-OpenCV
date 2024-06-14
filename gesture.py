ec097036bcd6750ed98eade87d31e833b945e314
Prakash-Kani <prakashkoffi@gmail.com>
2024-06-14 13:17:48 +0530

    Added Conditions for Task Swicher

--- gesture.py
+++ gesture.py
@@ -25,47 +25,47 @@ def to_find_index_finger_tip(landmarks):
 
 
 # Condition for Cursor Moments
-def to_move_cursor(finger_tip, distance):
+def to_move_cursor(finger_tip, distance, landmarks):
     if finger_tip is not None:
         x = int(finger_tip[0] * screen_width)
         y = int(finger_tip[1] * 1.1 * screen_height)
         if x and y and distance > 70:
-            return x, y, distance > 70
+            return x, y, distance > 70, get_angle(landmarks[5], landmarks[6], landmarks[8]) > 100
         else:
-            return x, y, False
+            return x, y, False, False
 
 
 # Condition for Left Click
-def to_left_click(landmarks, thump_dis):
+def to_left_click(landmarks, thumb_dis):
     return (get_angle(landmarks[5], landmarks[6], landmarks[8]) < 70 and 
             get_angle(landmarks[9], landmarks[10], landmarks[12]) > 150 and
             get_angle(landmarks[13], landmarks[14], landmarks[16]) < 70 and 
             get_angle(landmarks[17], landmarks[18], landmarks[20]) < 70 and
-             thump_dis < 70)
+             thumb_dis < 70)
     
 
 # Condition for Right Click
-def to_right_click(landmarks, thump_dis):
+def to_right_click(landmarks, thumb_dis):
     return (get_angle(landmarks[5], landmarks[6], landmarks[8]) > 150 and 
             get_angle(landmarks[9], landmarks[10], landmarks[12]) < 70  and
             get_angle(landmarks[13], landmarks[14], landmarks[16]) < 70 and 
             get_angle(landmarks[17], landmarks[18], landmarks[20]) < 70 and
-             thump_dis < 70)
+             thumb_dis < 70)
 
 
 # Condition for Double Click
-def to_double_click(landmarks, thump_dis):
+def to_double_click(landmarks, thumb_dis):
     return (get_angle(landmarks[5], landmarks[6], landmarks[8]) < 90  and 
             get_angle(landmarks[9], landmarks[10], landmarks[12]) < 90 and
-            get_angle(landmarks[13], landmarks[14], landmarks[16]) < 70 and 
-            get_angle(landmarks[17], landmarks[18], landmarks[20]) < 70 and
-             thump_dis < 70)
+            get_angle(landmarks[13], landmarks[14], landmarks[16]) > 70 and 
+            get_angle(landmarks[17], landmarks[18], landmarks[20]) > 70 and
+             thumb_dis < 70)
             
 # Condition for Display/Hide the Desktop
-def to_display_hide_desktop(landmarks, thump_dis):
+def to_display_hide_desktop(landmarks, thumb_dis):
     return (get_angle(landmarks[5], landmarks[6], landmarks[8])<60 and 
             get_angle(landmarks[9], landmarks[10], landmarks[12] ) < 60 and
-             thump_dis < 50)
+             thumb_dis < 50)
 
 # Condition for Drag and Drop
 def drag_and_drop_condition(landmarks):
@@ -85,3 +85,15 @@ def to_move_cursor1(finger_tip, distance):
         else:
             return x, y, False
   
+
+
+def to_task_swicher(landmarks, thumb_dis):
+    index = get_distance(landmarks[5], landmarks[8])
+    middle = get_distance(landmarks[9], landmarks[12])
+    ring = get_distance(landmarks[13], landmarks[16])
+    pinky = get_distance(landmarks[17], landmarks[20])
+    return (index < 60 and
+            middle < 60 and 
+            ring < 60 and 
+            pinky < 60 and 
+            thumb_dis > 90)
